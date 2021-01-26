c6bbc58f9344a54021a65e60b5cfa37f093582fb
daulet2030 <daulet2030@gmail.com>
2021-01-26 07:29:40 +0600

    Initial commit

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,26 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+    @Test
+    public void test1() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firefox() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void edge() {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+    }
+}
