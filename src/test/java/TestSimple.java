2e804366328c65e60065b89e46c6afc28a31df83
daulet2030 <daulet2030@gmail.com>
2021-01-26 08:42:36 +0600

    mvnw -Dtest=SimpleTest  test

---
+++ src/test/java/TestSimple.java
@@ -0,0 +1,32 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.annotations.Test;
+
+public class TestSimple {
+    @Test
+    public void test1() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+    }
+
+    @Test
+    public void firefox() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void edge() {
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver = new EdgeDriver();
+    }
+
+    @Test
+    public void safari() {
+        WebDriver driver = new SafariDriver();
+    }
+}
